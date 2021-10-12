6bad92c173a670b9519687f1786d0c8ebc356814
prajakta <prajaktamahajan789@gmail.com>
2021-10-12 17:21:10 +0530

    Added:Frequency of words in large paragraphs

--- src/main/java/com/bridgelabz/HashTable/HashTableMain.java
+++ src/main/java/com/bridgelabz/HashTable/HashTableMain.java
@@ -1,27 +1,29 @@
 package com.bridgelabz.HashTable;
 
+import java.util.HashMap;
+
 public class HashTableMain {
 
 
 	public static void main(String[] args) {
-		MyHashTable<String, Integer> hashTable = new MyHashTable();
-		String sentence = "To be or not to be";
+        HashMap <String, Integer> hashTable = new HashMap<String, Integer>();
+        String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
-		/* Storing given sentence in an array.
-       Converting the sentence to lower case and removing spaces.
-		 */
-		String[] sentenceArray = sentence.toLowerCase().split(" ");
+        /* Storing given sentence in an array.
+           Converting the sentence to lower case and removing spaces.
+        */
+        String[] sentenceArray = sentence.toLowerCase().split(" ");
 
-		// Iterating over the array.
-		for (String word : sentenceArray) {
-			Integer count = hashTable.get(word);
+        // Iterating over the array.
+        for (String word : sentenceArray) {
+            Integer count = hashTable.get(word);
 
-			if (count == null)
-				count = 1;
-			else
-				count = count + 1;
-			hashTable.add(word, count);
-		}
-		System.out.println(hashTable);
-	}
+            if (count == null)
+                count = 1;
+            else
+                count = count + 1;
+            hashTable.add(word, count);
+        }
+        System.out.println(hashTable);
+    }
 }
--- src/main/java/com/bridgelabz/HashTable/MyMapNode.java
+++ src/main/java/com/bridgelabz/HashTable/MyMapNode.java
@@ -4,46 +4,45 @@ public class MyMapNode  <K,V> {
 
 
 	K key;
-	V value;
-	MyMapNode<K, V> next;
+    V value;
+    MyMapNode<K, V> next;
 
-	public MyMapNode(K key, V value) {
-		this.key = key;
-		this.value = value;
-		next = null;
-	}
+    public MyMapNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+        next = null;
+    }
 
-	public K getKey() {
-		return key;
-	}
+    public K getKey() {
+        return key;
+    }
 
-	public void setKey(K key) {
-		this.key = key;
+    public void setKey(K key) {
+        this.key = key;
+    }
 
-	}
+    public V getValue() {
+        return value;
+    }
 
-	public V getValue() {
-		return value;
-	}
+    public void setValue(V value) {
+        this.value = value;
+    }
 
-	public void setValue(V value) {
-		this.value = value;
-	}
+    public MyMapNode<K, V> getNext() {
+        return next;
+    }
 
-	public MyMapNode<K, V> getNext() {
-		return next;
-	}
+    public void setNext(MyMapNode<K, V> next) {
+        this.next = next;
+    }
 
-	public void setNext(MyMapNode<K, V> next) {
-		this.next = (MyMapNode<K, V>) next;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder nodeString = new StringBuilder();
-		nodeString.append("MyMapNode{" + " Key= ").append(key).append(" Value= ").append(value).append('}');
-		if (next != null)
-			nodeString.append("->").append(next);
-		return nodeString.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuilder nodeString = new StringBuilder();
+        nodeString.append("Node{" + "Key=").append(key).append(" Value=").append(value).append("}");
+        if(next != null)
+            nodeString.append("->").append(next);
+        return nodeString.toString();
+    }
 }
