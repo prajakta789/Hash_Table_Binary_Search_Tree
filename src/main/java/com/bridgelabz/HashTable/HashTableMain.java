599a4c5eceaf8c9b0f99c7397235925c71130989
prajakta <prajaktamahajan789@gmail.com>
2021-10-12 17:30:19 +0530

    Added:Remove avoidable word from the phrase

--- src/main/java/com/bridgelabz/HashTable/HashTableMain.java
+++ src/main/java/com/bridgelabz/HashTable/HashTableMain.java
@@ -5,25 +5,27 @@ import java.util.HashMap;
 public class HashTableMain {
 
 
-	public static void main(String[] args) {
-        HashMap <String, Integer> hashTable = new HashMap<String, Integer>();
-        String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
-        /* Storing given sentence in an array.
-           Converting the sentence to lower case and removing spaces.
-        */
-        String[] sentenceArray = sentence.toLowerCase().split(" ");
+	    public static void main(String[] args) {
+	        HashMap hashTable = new HashMap();
+	        String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
-        // Iterating over the array.
-        for (String word : sentenceArray) {
-            Integer count = hashTable.get(word);
+	        /* Storing given sentence in an array.
+	           Converting the sentence to lower case and removing spaces.
+	        */
+	        String[] sentenceArray = sentence.toLowerCase().split(" ");
 
-            if (count == null)
-                count = 1;
-            else
-                count = count + 1;
-            hashTable.add(word, count);
-        }
-        System.out.println(hashTable);
-    }
-}
+	        // Iterating over the array.
+	        for (String word : sentenceArray) {
+	            Integer count = (Integer) hashTable.get(word);
+
+	            if (count == null)
+	                count = 1;
+	            else
+	                count = count + 1;
+	            hashTable.add(word, count);
+	        }
+	        hashTable.remove("avoidable");
+	        System.out.println(hashTable);
+	    }
+	}
--- src/main/java/com/bridgelabz/HashTable/MyHashTable.java
+++ src/main/java/com/bridgelabz/HashTable/MyHashTable.java
@@ -6,94 +6,114 @@ public class MyHashTable <K,V> {
 
 
 	MyMapNode<K, V> head;
-	MyMapNode<K, V> tail;
-	private int numOfBuckets;
-	ArrayList<MyMapNode<K, V>> myBucketArray;
+    MyMapNode<K, V> tail;
+    private int numOfBuckets = 0;
+    ArrayList<MyMapNode<K, V>> myBucketArray;
 
-	public void MyHashMap() {
-		this.numOfBuckets = 10;
-		this.myBucketArray = new ArrayList<MyMapNode<K, V>>(numOfBuckets);
-		// Create empty LinkedLists
-		for (int i = 0; i < numOfBuckets; i++)
-			this.myBucketArray.add(null);
-	}
+    public void MyHashMap() {
+        this.numOfBuckets = 10;
+        this.myBucketArray = new ArrayList<MyMapNode<K, V>>(numOfBuckets);
+        // Create empty LinkedLists
+        for (int i = 0; i < numOfBuckets; i++)
+            this.myBucketArray.add(null);
+    }
 
-	/**
-	 * Purpose : method to get value from LinkedList using index number
-	 * @param key : key is returned
-	 */
+    /**
+     * Purpose : method to get value from LinkedList using index number
+     * @param key : key is returned
+     */
 
-	public V get(K key) {
-		int index = this.getBucketIndex(key);
-		if (this.myBucketArray.get(index) == null)
-			return null;
-		MyMapNode<K, V> myNode = search(key);
-		return (myNode == null) ? null : myNode.getValue();
-	}
+    public V get(K key) {
+        int index = this.getBucketIndex(key);
+        if (this.myBucketArray.get(index) == null)
+            return null;
+        MyMapNode<K, V> myNode = search(key);
+        return (myNode == null) ? null : myNode.getValue();
+    }
 
-	/**
-	 * Purpose : Method to search the word in LinkedList
-	 * @param key : key to search
-	 */
+    /**
+     * Purpose : Method to search the word in LinkedList
+     * @param key : key to search
+     */
 
-	public MyMapNode<K, V> search(K key) {
-		MyMapNode<K, V> currentNode = head;
-		while (currentNode != null) {
-			if (currentNode.getKey().equals(key)) {
-				return currentNode;
-			}
-			currentNode = currentNode.getNext();
-		}
+    public MyMapNode<K, V> search(K key) {
+        MyMapNode<K, V> currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.getKey().equals(key)) {
+                return currentNode;
+            }
+            currentNode = currentNode.getNext();
+        }
 
-		return currentNode;
-	}
+        return currentNode;
+    }
 
-	/**
-	 * Purpose : Method to add key and value to hash table
-	 * @param key : word to be added
-	 * @param value: frequency of word
-	 */
-	public void add(K key, V value) {
-		int index = this.getBucketIndex(key);
-		MyMapNode<K, V> myNode = this.myBucketArray.get(index);
-		if (myNode == null) {
-			myNode = new MyMapNode<K, V>(key, value);
-			this.myBucketArray.set(index, myNode);
-		}
-		myNode = search(key);
-		if (myNode == null) {
-			myNode = new MyMapNode<K, V>(key, value);
-			this.append(myNode);
-		} else
-			myNode.setValue(value);
-	}
+    /**
+     * Purpose : Method to add key and value to hash table
+     * @param key : word to be added
+     * @param value: frequency of word
+     */
+    public void add(K key, V value) {
+        int index = this.getBucketIndex(key);
+        MyMapNode<K, V> myNode = this.myBucketArray.get(index);
+        if (myNode == null) {
+            myNode = new MyMapNode<K, V>(key, value);
+            this.myBucketArray.set(index, myNode);
+        }
+        myNode = search(key);
+        if (myNode == null) {
+            myNode = new MyMapNode<K, V>(key, value);
+            this.append(myNode);
+        } else
+            myNode.setValue(value);
+    }
 
-	// This implements hash function to find index for a key
-	public int getBucketIndex(K key) {
-		int hashCode = Math.abs(key.hashCode());
-		int index = hashCode % numOfBuckets;
-		return index;
+    // This implements hash function to find index for a key
+    public int getBucketIndex(K key) {
+        int hashCode = Math.abs(key.hashCode());
+        int index = hashCode % numOfBuckets;
+        return index;
 
-	}
+    }
 
-	/**
-	 * Purpose : Method to append value to Linked List
-	 * @param myNode : value to append
-	 */
+    /**
+     * Purpose : Method to append value to Linked List
+     * @param myNode : value to append
+     */
 
-	private void append(MyMapNode<K, V> myNode) {
-		if (this.head == null)
-			this.head = myNode;
-		if (this.tail == null)
-			this.tail = myNode;
-		else {
-			this.tail.setNext(myNode);
-			this.tail = myNode;
-		}
-	}
+    private void append(MyMapNode<K, V> myNode) {
+        if (this.head == null)
+            this.head = myNode;
+        if (this.tail == null)
+            this.tail = myNode;
+        else {
+            this.tail.setNext(myNode);
+            this.tail = myNode;
+        }
+    }
 
-	@Override
-	public String toString() {
-		return "MyHashMapNodes{" + head + '}';
-	}
+    /**
+     * Purpose : Method to remove a word
+     * @param key : word to be removed
+     */
+    public void remove(K key) {
+        MyMapNode<K, V> currentNode = head;
+        MyMapNode<K, V> previousNode = null;
+        while (currentNode != null && currentNode.getKey().equals(key)) {
+            head = currentNode.getNext();
+        }
+        while (currentNode != null && !(currentNode.getKey().equals(key))) {
+            previousNode = currentNode;
+            currentNode = currentNode.getNext();
+        }
+        if (currentNode != null)
+            previousNode.next = currentNode.next;
+        if (currentNode == null)
+            System.out.println("Word not found");
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashMapNodes{" + head + '}';
+    }
 }
