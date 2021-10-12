6d845dad6ff1bd1aa098d2a6026de4fcdebc0476
prajakta <prajaktamahajan789@gmail.com>
2021-10-12 18:04:15 +0530

    Added:Search element in binary tree

--- src/main/java/com/Binary_Search_Tree/BinaryNode.java
+++ src/main/java/com/Binary_Search_Tree/BinaryNode.java
@@ -13,4 +13,3 @@ public class BinaryNode<K extends Comparable<K>> {
 } 
 
 
-
--- src/main/java/com/Binary_Search_Tree/BinaryTree.java
+++ src/main/java/com/Binary_Search_Tree/BinaryTree.java
@@ -1,49 +1,69 @@
 package com.Binary_Search_Tree;
 
 public class BinaryTree <K extends Comparable<K>> {
-		    BinaryNode<K> root;
+	BinaryNode<K> root;
 
-		    public void add(int Key) {
-		        BinaryNode<K> newNode = new BinaryNode<K>(Key);
-		        if (root == null) {
-		            root = newNode;
-		            return;
-		        } else {
-		            BinaryNode<K> current = root;
-		            BinaryNode<K> parent = null;
-		            while (true) {
-		                parent = current;
-		                // if key less than root assigning to left
-		                if (Key < current.key) {
-		                    current = current.left;
-		                    if (current == null) {
-		                        parent.left = newNode;
-		                        return;
-		                    }
-		                }
-		                // Assigning key to right
-		                else {
-		                    current = current.right;
-		                    if (current == null) {
-		                        parent.right = newNode;
-		                        return;
-		                    }
-		                }
-		            }
-		        }
-		    }
+    public void add(int Key) {
+        BinaryNode<K> newNode = new BinaryNode<K>(Key);
+        if (root == null) {
+            root = newNode;
+            return;
+        } else {
+            BinaryNode<K> current = root;
+            BinaryNode<K> parent = null;
+            while (true) {
+                parent = current;
+                // if key less than root assigning to left
+                if (Key < current.key) {
+                    current = current.left;
+                    if (current == null) {
+                        parent.left = newNode;
+                        return;
+                    }
+                }
+                // Assigning key to right
+                else {
+                    current = current.right;
+                    if (current == null) {
+                        parent.right = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
 
-		    /* This Method to display the Binary Search Tree */
-		    public void print(BinaryNode<K> node) {
-		        if (node == null) {
-		            System.out.println("Tree is empty");
-		        } else {
-		            if (node.left != null)
-		                print(node.left);
-		            System.out.print(node.key + " ");
-		            if (node.right != null)
-		                print(node.right);
+    /* This Method to display the Binary Search Tree */
+    public void print(BinaryNode<K> node) {
+        if (node == null) {
+            System.out.println("Tree is empty");
+        } else {
+            if (node.left != null)
+                print(node.left);
+            System.out.print(node.key + " ");
+            if (node.right != null)
+                print(node.right);
 
-		        }
-		    }
-		}
+        }
+    }
+    public BinaryNode<K> searchNode(BinaryNode<K> root, int key) {
+        // root is null or key is present at root
+        if (root == null || root.key == key)
+            return root;
+        // value is greater than key
+        if (root.key > key)
+            return searchNode(root.left, key);
+        // value is less than key
+        return searchNode(root.right, key);
+
+    }
+
+    public boolean search(int value) {
+        root = searchNode(root, value);
+        if (root != null)
+            return true;
+        else
+            return false;
+
+    }
+}
